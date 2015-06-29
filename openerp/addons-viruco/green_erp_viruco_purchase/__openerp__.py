bc30a14b1adbb6dce5970f9260b4d0c85be16a53
Phung Pham <minhphung171093@gmail.com>
2015-06-29 08:32:43 +0700

    xong chuc nang tach hop dong cho DO

--- openerp/addons-viruco/green_erp_viruco_purchase/__openerp__.py
+++ openerp/addons-viruco/green_erp_viruco_purchase/__openerp__.py
@@ -25,7 +25,7 @@
     'description': """Purchase""",
     'author': "tranhung07081989@gmail.com",
     'website': "www.acespritech.com",
-    'depends': ['purchase','green_erp_viruco_sale'],
+    'depends': ['purchase','green_erp_viruco_sale','report_aeroo','report_aeroo_ooo'],
     'data': [
         'report/don_mua_hang_view.xml',
         'purchase_view.xml',
--- openerp/addons-viruco/green_erp_viruco_stock/wizard/__init__.py
+++ openerp/addons-viruco/green_erp_viruco_stock/wizard/__init__.py
@@ -1,8 +1,8 @@
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    Acespritech Solutions Pvt. Ltd.
-#    Copyright (C) 2013-2014
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -18,8 +18,7 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
+
+import stock_move
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
-import config
-import stock
-import wizard
-import report
+
