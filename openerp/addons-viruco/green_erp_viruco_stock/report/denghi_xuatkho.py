35ff63909f7bc5a991c66caebe6b6e78c8c57e19
Phung Pham <minhphung171093@gmail.com>
2015-06-29 10:14:48 +0700

    update

---
+++ openerp/addons-viruco/green_erp_viruco_stock/report/denghi_xuatkho.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    HLVSolution, Open Source Management Solution
+#
+##############################################################################
+import time
+from openerp.report import report_sxw
+from openerp import pooler
+from openerp.osv import osv
+from openerp.tools.translate import _
+import random
+DATETIME_FORMAT = "%Y-%m-%d %H:%M:%S"
+DATE_FORMAT = "%Y-%m-%d"
+
+from openerp.tools import DEFAULT_SERVER_DATE_FORMAT, DEFAULT_SERVER_DATETIME_FORMAT, float_compare
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
+from green_erp_viruco_sale.report import amount_to_text_vn
+class Parser(report_sxw.rml_parse):
+        
+    def __init__(self, cr, uid, name, context):
+        super(Parser, self).__init__(cr, uid, name, context=context)
+        pool = pooler.get_pool(self.cr.dbname)
+        self.localcontext.update({
+            'convert_f_amount':self.convert_f_amount,
+        })
+        
+    def convert_f_amount(self, amount):
+        a = format(amount,',')
+        b = a.split('.')
+        if len(b)==2 and len(b[1])==1:
+            a+='0'
+        return a.replace(',',' ')
+    
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
