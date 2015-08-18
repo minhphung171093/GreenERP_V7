4ee23ea337dd3c3e66761994d968ba12a788a7e8
TrongNguyen <nguyentrong6193@gmail.com>
2015-08-18 17:15:11 +0700

    update

--- openerp/addons-ccty/green_erp_ccty_vaccine/ql_vaccine.py
+++ openerp/addons-ccty/green_erp_ccty_vaccine/ql_vaccine.py
@@ -22,8 +22,10 @@ class nhap_vaccine(osv.osv):
     _columns = {
         'name': fields.many2one('loai.vacxin','Loại vaccine', required = True, states={ 'done':[('readonly', True)]}),
         'can_bo_id': fields.many2one('res.users','Cán bộ nhập máy', required = True, states={ 'done':[('readonly', True)]}),
-        'ngay_nhap': fields.date('Ngày nhập xuất', states={ 'done':[('readonly', True)]}),
+        'ngay_nhap': fields.date('Ngày nhập', states={ 'done':[('readonly', True)]}),
         'soluong': fields.char('Số lượng',size = 50, states={ 'done':[('readonly', True)]}),
+        'so_lo_id':fields.many2one('so.lo','Số lô', required = True, states={ 'done':[('readonly', True)]}),
+        'han_su_dung':fields.related('so_lo_id','han_su_dung',type='date',string='HSD đến'),
         'state':fields.selection([('draft', 'Nháp'),('done', 'Duyệt')],'Status', readonly=True),
                 }
     _defaults = {
@@ -31,8 +33,16 @@ class nhap_vaccine(osv.osv):
                  }
     def bt_duyet(self, cr, uid, ids, context=None):
         return self.write(cr, uid, ids,{'state':'done'})
+    
 nhap_vaccine()
 
-    
+class so_lo(osv.osv):
+    _name = "so.lo"
+    _columns = {
+        'name': fields.char('Số lô',size = 50),
+        'han_su_dung':fields.date('HSD đến ngày'),
+        'vacxin_id': fields.many2one('loai.vacxin','Loại vaccine', required = True),
+                }
+so_lo()   
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
