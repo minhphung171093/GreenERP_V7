47cef4ed84b9d56996368e38db20cc7380d76a11
phuoc <your_email@example.com>
2015-09-07 16:44:41 +0700

    update

--- openerp/addons-qldh/green_erp_qldh/qldh.py
+++ openerp/addons-qldh/green_erp_qldh/qldh.py
@@ -72,7 +72,7 @@ class ct_nhom_cong_viec(osv.osv):
         return True
     _columns = {
         'nhom_cv_id':fields.many2one('nhom.cong.viec','nhom cong viec',ondelete='cascade'),
-        'name':fields.char('Tên chi tiết',size=1024),
+        'name':fields.char('Tên chi tiết',size=1024, required = True),
         'datas_fname': fields.char('File Name',size=256),
         'datas': fields.function(_data_get, fnct_inv=_data_set, string='File Content', type="binary", nodrop=True),
         'store_fname': fields.char('Stored Filename', size=256),
@@ -98,11 +98,52 @@ quy_trinh()
 
 class buoc_thuc_hien_line(osv.osv):
     _name = "buoc.thuc.hien.line"
+    
+    def _data_get(self, cr, uid, ids, name, arg, context=None):
+        if context is None:
+            context = {}
+        result = {}
+        location = self.pool.get('ir.config_parameter').get_param(cr, SUPERUSER_ID, 'ir_attachment.location')
+        bin_size = context.get('bin_size')
+        for attach in self.browse(cr, uid, ids, context=context):
+            if location and attach.store_fname:
+                result[attach.id] = self._file_read(cr, uid, location, attach.store_fname, bin_size)
+            else:
+                result[attach.id] = attach.db_datas
+                if bin_size:
+                    result[attach.id] = int(result[attach.id])
+
+        return result
+
+    def _data_set(self, cr, uid, id, name, value, arg, context=None):
+        # We dont handle setting data to null
+        if not value:
+            return True
+        if context is None:
+            context = {}
+        location = self.pool.get('ir.config_parameter').get_param(cr, SUPERUSER_ID, 'ir_attachment.location')
+        file_size = len(value.decode('base64'))
+        if location:
+            attach = self.browse(cr, uid, id, context=context)
+            if attach.store_fname:
+                self._file_delete(cr, uid, location, attach.store_fname)
+            fname = self._file_write(cr, uid, location, value)
+            # SUPERUSER_ID as probably don't have write access, trigger during create
+            super(ct_nhom_cong_viec, self).write(cr, SUPERUSER_ID, [id], {'store_fname': fname, 'file_size': file_size}, context=context)
+        else:
+            super(ct_nhom_cong_viec, self).write(cr, SUPERUSER_ID, [id], {'db_datas': value, 'file_size': file_size}, context=context)
+        return True
+    
     _columns = {
         'quy_trinh_id':fields.many2one('quy.trinh','Quy trình', ondelete = 'cascade'),
         'name': fields.char('Tên chi tiết', size = 100, required=True),
         'yeu_cau_kq':fields.text('Yêu cầu kết quả đạt được'),
         'cach_thuc_hien': fields.text('Cách thức thực hiện'),
+        'datas_fname': fields.char('File Name',size=256),
+        'datas': fields.function(_data_get, fnct_inv=_data_set, string='File Content', type="binary", nodrop=True),
+        'store_fname': fields.char('Stored Filename', size=256),
+        'db_datas': fields.binary('Database Data'),
+        'file_size': fields.integer('File Size'),
                 }
     
 quy_trinh()
