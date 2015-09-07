d50a19cd524118ccae61ae43997e150ece6c17a6
phuoc <your_email@example.com>
2015-09-07 10:57:17 +0700

    update

--- openerp/addons-qldh/green_erp_qldh/qldh.py
+++ openerp/addons-qldh/green_erp_qldh/qldh.py
@@ -15,8 +15,16 @@ import codecs
 import os
 # from xlrd import open_workbook,xldate_as_tuple
 from openerp import modules
-base_path = os.path.dirname(modules.get_module_path('green_erp_qldh_base'))
 
 
-
+class quy_trinh(osv.osv):
+    _name = "quy.trinh"
+    _columns = {
+        'name': fields.char('Tên quy trình', size = 100, required=True),
+        'phong_ban_id':fields.many2one('phong.ban','Phòng Ban',required = True),
+        'chuc_vu_id':fields.many2one('chuc.vu','Chức vụ'),
+        'buoc_thuc_hien_line':fields.one2many('buoc_thuc_hien_line','quy_trinh_id','Các bước thực hiện'),
+                }
+    
+quy_trinh()
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
--- openerp/addons-qldh/green_erp_qldh_base/danhmuc.py
+++ openerp/addons-qldh/green_erp_qldh_base/danhmuc.py
@@ -18,5 +18,29 @@ from openerp import modules
 base_path = os.path.dirname(modules.get_module_path('green_erp_qldh_base'))
 
 
+class phong_ban(osv.osv):
+    _name = "phong.ban"
+    _columns = {
+        'name': fields.char('Tên phòng ban', size = 100, required=True),
+        'truong_phong_id': fields.many2one('nhan.vien','Trưởng phòng', required=True),
+        'ds_nhan_vien_line': fields.one2many('ds.nhan.vien.line','phong_ban_id','Các nhân viên'),
+                }
+phong_ban()
 
+class ds_nhan_vien_line(osv.osv):
+    _name = "ds.nhan.vien.line"
+    _columns = {
+        'phong_ban_id': fields.many2one('phong.ban','Phòng ban', ondelete='cascade'),
+        'nhan_vien_id': fields.many2one('nhan.vien','Nhân viên', required=True),
+        'chuc_vu_id': fields.many2one('chuc.vu','Chức vụ', readonly = True),
+                }
+ds_nhan_vien_line()
+
+class nhan_vien(osv.osv):
+    _name = "nhan.vien"
+    _columns = {
+        'name': fields.char('Tên nhân viên', size = 100, required=True),
+        'chuc_vu_id': fields.many2one('chuc.vu','Chức vụ', required=True),
+                }
+nhan_vien()
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
