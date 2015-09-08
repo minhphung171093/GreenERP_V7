fe54a2c10c095e3ec3fe62635c5ea17a7182c2a1
TrongNguyen <nguyentrong6193@gmail.com>
2015-09-08 08:32:11 +0700

    update

--- openerp/addons-qldh/green_erp_qldh/qldh.py
+++ openerp/addons-qldh/green_erp_qldh/qldh.py
@@ -27,11 +27,81 @@ class nhom_cong_viec(osv.osv):
         'trang_thai':fields.selection([('moi','Mới nhận'),('lam','Đang làm'),('cho_duyet','Chờ phê duyệt'),('duyet','Đã duyệt')],'Trạng thái'),
         'ct_nhom_cv_line':fields.one2many('ct.nhom.cong.viec','nhom_cv_id','Chi tiết công việc'),
         'ghi_chu':fields.text('Ghi chú'),
+        'cong_viec_line':fields.one2many('cong.viec','cong_viec_id','Công việc'),
+                
+    }
+    _defaults = {
+             'ho_tro':'co'    
+                 }
+nhom_cong_viec()
+
+class cong_viec(osv.osv):
+    _name = "cong.viec"
+    _columns = {
+        'cong_viec_id':fields.many2one('nhom.cong.viec','Công việc'),
+        'name':fields.char('Tên công việc',size=1024,required=True),
+        'phong_ban_id':fields.many2one('phong.ban','Phòng ban'),
+#         'ho_tro':fields.selection([('co','Có'),('khong','Không')],'Hỗ trợ'),
+        'quy_trinh_id':fields.many2one('quy.trinh','Quy trình'),
+        'trang_thai':fields.selection([('moi','Mới nhận'),('lam','Đang làm'),('cho_duyet','Chờ phê duyệt'),('duyet','Đã duyệt')],'Trạng thái'),
+        'chi_tiet_cv_line':fields.one2many('chi.tiet.cong.viec','nhom_cv_id','Chi tiết công việc'),
+        'ghi_chu':fields.text('Ghi chú'),
 #         'cong_viec_line':fields.one2many('ct.nhom.cong.viec','nhom_cv_id','Chi tiết công việc'),
                 
     }
+cong_viec()
 
-nhom_cong_viec()
+class chi_tiet_cong_viec(osv.osv):
+    _name = "chi.tiet.cong.viec"
+    
+    def _data_get(self, cr, uid, ids, name, arg, context=None):
+        if context is None:
+            context = {}
+        result = {}
+        location = self.pool.get('ir.config_parameter').get_param(cr, SUPERUSER_ID, 'ir_attachment.location')
+        bin_size = context.get('bin_size')
+        for attach in self.browse(cr, uid, ids, context=context):
+            if location and attach.store_fname:
+                result[attach.id] = self._file_read(cr, uid, location, attach.store_fname, bin_size)
+            else:
+                result[attach.id] = attach.db_datas
+                if bin_size:
+                    result[attach.id] = int(result[attach.id])
+
+        return result
+
+    def _data_set(self, cr, uid, id, name, value, arg, context=None):
+        # We dont handle setting data to null
+        if not value:
+            return True
+        if context is None:
+            context = {}
+        location = self.pool.get('ir.config_parameter').get_param(cr, SUPERUSER_ID, 'ir_attachment.location')
+        file_size = len(value.decode('base64'))
+        if location:
+            attach = self.browse(cr, uid, id, context=context)
+            if attach.store_fname:
+                self._file_delete(cr, uid, location, attach.store_fname)
+            fname = self._file_write(cr, uid, location, value)
+            # SUPERUSER_ID as probably don't have write access, trigger during create
+            super(chi_tiet_cong_viec, self).write(cr, SUPERUSER_ID, [id], {'store_fname': fname, 'file_size': file_size}, context=context)
+        else:
+            super(chi_tiet_cong_viec, self).write(cr, SUPERUSER_ID, [id], {'db_datas': value, 'file_size': file_size}, context=context)
+        return True
+    _columns = {
+        'cv_id':fields.many2one('cong.viec','Cong viec',ondelete='cascade'),
+        'name':fields.char('Tên chi tiết',size=1024),
+        'datas_fname': fields.char('File Name',size=256),
+        'datas': fields.function(_data_get, fnct_inv=_data_set, string='File Content', type="binary", nodrop=True),
+        'store_fname': fields.char('Stored Filename', size=256),
+        'db_datas': fields.binary('Database Data'),
+        'file_size': fields.integer('File Size'),
+        'yeu_cau_kq':fields.text('Yêu cầu kết quả'),
+        'cach_thuc_hien':fields.text('Cách thức thực hiện'),
+
+    }
+
+chi_tiet_cong_viec()
 
 class ct_nhom_cong_viec(osv.osv):
     _name = "ct.nhom.cong.viec"
