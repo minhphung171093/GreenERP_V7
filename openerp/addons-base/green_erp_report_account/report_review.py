ce7812b3014c2902738180991a34c40e04c97dd1
tien <tien@tien-123.(none)>
2015-10-02 17:24:13 +0700

    up

--- openerp/addons-base/green_erp_report_account/report_review.py
+++ openerp/addons-base/green_erp_report_account/report_review.py
@@ -87,4 +87,65 @@ class account_ledger_report_review_line(osv.osv):
      
 account_ledger_report_review_line()
 
+class so_quy_review(osv.osv):
+    _name = "so.quy.review"
+ 
+    _columns = {
+        'name': fields.char('Name', size=1024),
+        'don_vi': fields.char('Đơn vị', size=1024),
+        'dia_chi': fields.char('Địa chỉ', size=1024),
+        'account_code': fields.char('Tài khoản', size=1024),
+        'account_name': fields.char('Ten Tài khoản', size=1024),
+        'date_from': fields.char('Từ ngày', size=1024),
+        'date_to': fields.char('đến ngày', size=1024),
+        'so_quy_line' : fields.one2many('so.quy.review.line','so_quy_review_id','So Quy Line'),        
+    }
+     
+    def print_xls(self, cr, uid, ids, context=None):
+        if context is None:
+            context = {}
+        datas = {'ids': ids}
+        datas['model'] = 'so.quy.review'
+        datas['form'] = self.read(cr, uid, ids)[0]
+        datas['form'].update({'active_id':ids[0]})
+        return {'type': 'ir.actions.report.xml', 'report_name': 'so_quy_chung_review_report', 'datas': datas}
+    
+    def print_detail_xls(self, cr, uid, ids, context=None):
+        if context is None:
+            context = {}
+        datas = {'ids': ids}
+        datas['model'] = 'so.quy.review'
+        datas['form'] = self.read(cr, uid, ids)[0]
+        datas['form'].update({'active_id':ids[0]})
+        return {'type': 'ir.actions.report.xml', 'report_name': 'soquy_tienmat_chitiet_review_report', 'datas': datas}
+     
+    def print_tienmat_xls(self, cr, uid, ids, context=None):
+        if context is None:
+            context = {}
+        datas = {'ids': ids}
+        datas['model'] = 'so.quy.review'
+        datas['form'] = self.read(cr, uid, ids)[0]
+        datas['form'].update({'active_id':ids[0]})
+        return {'type': 'ir.actions.report.xml', 'report_name': 'so_tiengui_nganhang_review_report', 'datas': datas}
+so_quy_review()
+
+class so_quy_review_line(osv.osv):
+    _name = "so.quy.review.line"
+ 
+    _columns = {
+        'so_quy_review_id': fields.many2one('so.quy.review', 'so quy', readonly=True),
+        'ngay_ghso': fields.char('Ngày, tháng ghi sổ', size=1024),
+        'ngay_thang': fields.char('Ngày tháng chứng từ ', size=1024),
+        'so_hieu_thu': fields.char('Số hiệu chứng từ Thu', size=1024),
+        'so_hieu_chi': fields.char('Số hiệu chứng từ Chi', size=1024),
+        'dien_giai': fields.char('Diễn giải', size=1024),
+        'tk_doi_ung': fields.char('Tài khoản đối ứng', size=1024),
+        'so_ps_no': fields.float('Số phát sinh Nợ', readonly=True),
+        'so_ps_co': fields.float('Số phát sinh Có', readonly=True),
+        'so_ps_ton': fields.float('Số phát sinh Tồn', readonly=True),
+        'ghi_chu': fields.char('Ghi chú', size=1024),
+    }
+     
+so_quy_review_line()
+
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
